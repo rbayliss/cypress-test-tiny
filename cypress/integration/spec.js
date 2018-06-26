788a9c42e9232ea8a2d3c26a63f96c097a9c033b
Rob Bayliss <rob@lastcallmedia.com>
2018-06-26 09:18:10 -0400

    Minimal test case

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,3 +1,12 @@
 describe('page', () => {
-  it('works', () => {})
+  it('works', () => {
+    cy.visit('https://www.mass.gov/guides/massachusetts-identification-id-requirements');
+    cy.on('uncaught:exception', function(err) {
+      // Ignore known GTM error.
+      if(err.message.startsWith('$ is not a function')) {
+          return false;
+      }
+    });
+    cy.get('#main-content').screenshot();
+  })
 })
